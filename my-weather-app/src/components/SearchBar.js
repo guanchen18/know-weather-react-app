fec2680f34326d2db6748de00a4a0881c8d2801e
Guanchen <guanchen.song.797@gmail.com>
2020-01-22 15:46:25 +0100

    Add onSubmit

--- my-weather-app/src/components/SearchBar.js
+++ my-weather-app/src/components/SearchBar.js
@@ -4,9 +4,10 @@ class SearchBar extends Component {
 
     render (){
         return (
-            <form>
+            <form onSubmit={this.props.handleClick}> 
                 <input type="text" name="city" placeholder="city name"></input>
                 <input type="text" name="country" placeholder="country name"></input>
+                {/* <p>{this.props}</p> */}
                 <button>SET</button>
             </form>
         )
