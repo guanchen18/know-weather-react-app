733353b8ffa0b25a77692c0854aea6818420e9a5
Guanchen <guanchen.song.797@gmail.com>
2020-01-22 15:51:46 +0100

    Fetch api data

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SearchBar from './components/SearchBar';
 
 
-function App() {
-  return (
-    <div >
-    <h1>Hello, world!</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    temperature: null,
+    city: '',
+    country: '',
+    humidity: null,
+    description: null,
+    error: null
+  }
+  getWeather = async (e) => {
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    // const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${Api_Key}`);
+    const response = await api_call.json();
+    console.log(response);
+  }
+  render(){
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <SearchBar handleClick={this.getWeather}/>
+      </div>
+    )
+  }
 }
 
 export default App;
+
