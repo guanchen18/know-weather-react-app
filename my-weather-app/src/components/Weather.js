0ac3db408be7e291eb684b008ef422286c20c05f
Guanchen <guanchen.song.797@gmail.com>
2020-01-22 17:06:06 +0100

    Add today date

--- my-weather-app/src/components/Weather.js
+++ my-weather-app/src/components/Weather.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import dateformat from 'dateformat';
+const today = new Date();
 
 class Weather extends Component {
+
     render () {
         return (
             <div>
-                <span>{this.props.city}, {this.props.country}</span>
-                <span>{this.props.temperature}</span>
+                <p>
+                    <span>{this.props.city}, {this.props.country}</span>
+                </p> 
+                <p>
+                    <span>{this.props.temperature}</span>
+                </p>   
+                <p>
+                    <span>{this.props.description}</span>
+                </p>
 
+                <p><span>{dateformat(today, "dddd, mmmm dd")} </span></p>
 
             </div>
         )
