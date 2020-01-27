669b5393fb79e3440d2cc945441d46214ee20558
Guanchen <guanchen.song.797@gmail.com>
2020-01-27 15:42:22 +0100

    Modify button in SearchBar

--- my-weather-app/src/components/SearchBar.js
+++ my-weather-app/src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import searchbar from './SearchBar.module.css';
 import inputfield from '../elements/InputField.module.css';
+import button from '../elements/Button.module.css';
 
 
 class SearchBar extends Component {
@@ -12,7 +13,7 @@ class SearchBar extends Component {
                         <label type="text">Location</label>
                         <input type="text" name="city" placeholder="Enter a city" onChange={this.props.searchBarHandler}></input>
                         <input type="text" name="country" placeholder="Enter a country"></input>
-                        <button position="onForm" name="searchSubmit" type="submit" onClick={this.props.showUpdate}>SET</button>
+                        <button className={button.ButtonWrapper} position="onForm" name="searchSubmit" type="submit" onClick={this.props.showUpdate}>SET</button>
                     </div>
                 </form>
              
