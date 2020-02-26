423c6080f2ccf96c84f99de146e9873f06ed6641
Guanchen <guanchen.song.797@gmail.com>
2020-02-26 23:10:40 +0100

    Remove country search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,7 +12,7 @@ class SearchBar extends Component {
                     <div className={inputfield.InputFieldWrapper} >
                         <label type="text">Location</label>
                         <input type="text" name="city" placeholder="Enter a city" onChange={this.props.searchBarHandler}></input>
-                        <input type="text" name="country" placeholder="Enter a country"></input>
+                        {/* <input type="text" name="country" placeholder="Enter a country"></input> */}
                         <button className={button.ButtonWrapper} position="onForm" name="searchSubmit" type="submit" onClick={this.props.showUpdate}>SET</button>
                     </div>
                 </form>
