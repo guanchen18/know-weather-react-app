8dd011efa6a12b5d56fb39748ab941165c4cf9a9
Guanchen Song <guanchen.song.797@gmail.com>
2020-03-03 23:36:03 +0100

    Convert to function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+
 import searchbar from './SearchBar.module.css';
 import inputfield from '../elements/InputField.module.css';
 import button from '../elements/Button.module.css';
 
-
-class SearchBar extends Component {
-    render (){
-        return (
-            <div className={searchbar.SearchBarWrapper}>
-                <form onSubmit={this.props.handleClick}> 
-                    <div className={inputfield.InputFieldWrapper} >
-                        <label type="text">Location</label>
-                        <input type="text" name="city" placeholder="Enter a city" onChange={this.props.searchBarHandler}></input>
-                        {/* <input type="text" name="country" placeholder="Enter a country"></input> */}
-                        <button className={button.ButtonWrapper} position="onForm" name="searchSubmit" type="submit" onClick={this.props.showUpdate}>SET</button>
-                    </div>
-                </form>
-             
-            </div>
-        )
-        
-    }
+function SearchBar(props) {
+    return (
+        <div className={searchbar.SearchBarWrapper}>
+            <form onSubmit={props.handleClick}> 
+                <div className={inputfield.InputFieldWrapper} >
+                    <label type="text">Location</label>
+                    <input type="text" name="city" placeholder="Enter a city" onChange={props.searchBarHandler}></input>
+                    {/* <input type="text" name="country" placeholder="Enter a country"></input> */}
+                    <button className={button.ButtonWrapper} position="onForm" name="searchSubmit" type="submit" onClick={props.showUpdate}>SET</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default SearchBar;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,48 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import dateformat from 'dateformat';
+
+import Icon from '../elements/Icons/Icon';
+
+import date from './Date.module.css';
 import weather from './Weather.module.css';
 import temperature from './Temperature.module.css';
-import date from './Date.module.css';
 import description from './Description.module.css';
-import Icon from '../elements/Icons/Icon';
 import location from './Location.module.css';
 
-const today = new Date();
-
-class Weather extends Component {
-
-    render () {
-        const show = this.props.show;
-        // console.log(show)
-        if (show === true) {
+function Weather(props) {
+    const today = new Date();
+    if (props.show === true) {
+        if (props.error === null) {
             return (
                 <div className={weather.WeatherDetailsWrappe}>
 
                     <div>
-                        <Icon type={this.props.description} /> 
+                        <Icon type={props.description} /> 
                     </div>
 
                     <div className={weather.WeatherDataWrapper}>
 
-                    <div className={temperature.TemperatureWrapper}>
-                        <span>{Math.round(this.props.temperature)}</span><span className={temperature.TemperatureSymbol}>°</span>
-                    </div>  
+                        <div className={temperature.TemperatureWrapper}>
+                            <span>{Math.round(props.temperature)}</span><span className={temperature.TemperatureSymbol}>°</span>
+                        </div>  
 
-                    <div className={description.DescriptionWrapper}>
-                        {this.props.description}
-                    </div>
+                        <div className={description.DescriptionWrapper}>
+                            {props.description}
+                        </div>
 
-                    <div className={date.DateWrapper}><span>{dateformat(today, "dddd, mmmm dd")} </span></div>
-                    
-                    <div className={location.LocationWrapper}>
-                        <span>{this.props.city} {this.props.country}</span>
-                    </div> 
+                        <div className={date.DateWrapper}><span>{dateformat(today, "dddd, mmmm dd")} </span></div>
+                        
+                        <div className={location.LocationWrapper}>
+                            <span>{props.city} {props.country}</span>
+                        </div> 
                     </div>
 
                 </div>
-
             )
-        } else {return <p></p>}
+        }
+        else {
+            return(
+                <div className="error">
+                    {props.error}
+                </div>
+            )
+        }
+    }
+    else {
+        return <p></p>
     }
 }
 
